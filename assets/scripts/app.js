7a7db13d04aa70d3b076aa5e3174d98bca71dda0
babboe1 <ayodelaustinab@gmail.com>
2021-12-08 09:27:58 +0100

    refactor: create DOM variable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,4 +3,8 @@ const MENU = document.querySelector(".Menu");
 const MENU_BTN = document.querySelector(".Menu").firstElementChild;
 const CLOSE_MENU = document.querySelector(".CloseMenu");
 const CLOSE_MENU_BTN = document.querySelector(".CloseMenu").firstElementChild;
+const MENU_MODAL = document.getElementById("menu_modal").parentElement;
+const BACK_PROJECT_BTN = document.querySelector('.BackProjectBox');
+const PROJECT_DETAILS_BTN = document.getElementById('project_detail').parentElement;
+const CLOSE_MODAL_BTN = document.getElementById("close_modal").lastElementChild;
 
