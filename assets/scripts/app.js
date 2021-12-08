d696fa05f5208a5f9a7d028149a5a5ed2a0c576c
babboe1 <ayodelaustinab@gmail.com>
2021-12-08 09:27:13 +0100

    refactor: create Dom constants

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,6 @@
 const BACKDROP = document.getElementById('backdrop');
+const MENU = document.querySelector(".Menu");
+const MENU_BTN = document.querySelector(".Menu").firstElementChild;
+const CLOSE_MENU = document.querySelector(".CloseMenu");
+const CLOSE_MENU_BTN = document.querySelector(".CloseMenu").firstElementChild;
+
