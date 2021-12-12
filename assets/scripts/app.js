36f4be9a1e6a747526de0b82021a7fc76669f33e
babboe1 <ayodelaustinab@gmail.com>
2021-12-12 20:53:23 +0100

    refactor: format code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -98,11 +98,7 @@ const BorderColorFunction = () => {
 		PLEDGE_MODAL1.classList.remove('Borderline');
 		PLEDGE_MODAL2.classList.remove('Borderline');
 		PLEDGE_MODAL3.classList.add('Borderline');
-	} else {
-		PLEDGE_MODAL1.classList.remove('Borderline');
-		PLEDGE_MODAL2.classList.remove('Borderline');
-		PLEDGE_MODAL3.classList.remove('Borderline');
-	}
+   }
 };
 
 const InputHandler = () => {
